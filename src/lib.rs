5f601e03eb4a688bd3040a5cec3cae694f5f169a
SpiralP <spiral.palmer@gmail.com>
2019-08-28 10:33:14 -0700

    add a get_content_type function, strip leading / and default to index.html if blank string

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,27 @@ use std::{borrow, io};
 
 include!(concat!(env!("OUT_DIR"), "/web_files.rs"));
 
-pub fn get_file(file_path: &'static str) -> Result<borrow::Cow<'static, [u8]>, io::Error> {
+pub fn get_file(mut file_path: &str) -> Result<borrow::Cow<'static, [u8]>, io::Error> {
+  if let Some(c) = file_path.chars().nth(0) {
+    if c == '/' {
+      file_path = &file_path[1..];
+    }
+  }
+  if file_path.is_empty() {
+    file_path = "index.html";
+  }
+
   WEB_FILES.get(&format!("dist/{}", file_path))
+}
+
+pub fn get_content_type(file_path: &str) -> Option<String> {
+  if file_path.ends_with(".css") {
+    Some("text/css; charset=utf-8".to_string())
+  } else if file_path.ends_with(".js") {
+    Some("application/javascript; charset=utf-8".to_string())
+  } else if file_path.ends_with(".html") {
+    Some("text/html; charset=utf-8".to_string())
+  } else {
+    None
+  }
 }
