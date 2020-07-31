52d0f5d98724c8340cd51e5d036768035acdb858
SpiralP <SpiralP@users.noreply.github.com>
2020-07-31 07:39:31 +0000

    deploy: 496cb69e776d13ba886f6380940621e347c12db7

--- implementors/actix_codec/trait.Decoder.js
+++ implementors/actix_codec/trait.Decoder.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["actix_codec"] = [{"text":"impl <a class=\"trait\" href=\"actix_codec/trait.Decoder.html\" title=\"trait actix_codec::Decoder\">Decoder</a> for <a class=\"struct\" href=\"actix_codec/struct.BytesCodec.html\" title=\"struct actix_codec::BytesCodec\">BytesCodec</a>","synthetic":false,"types":["actix_codec::bcodec::BytesCodec"]}];
+implementors["actix_codec"] = [{"text":"impl Decoder for BytesCodec","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
